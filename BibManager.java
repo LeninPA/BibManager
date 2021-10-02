855c9f9c2b1aa756cc40b0e449dd30493c6939fd
paint-brush <84360200+paint-brush@users.noreply.github.com>
2021-10-01 23:27:42 -0700

    Add files via upload

---
+++ BibManager.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+//since we're making it simple, arraylists should be fine, right?
+import java.util.Scanner;
+public class BibManager {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
